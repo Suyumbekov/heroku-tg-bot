ec24fde0b41ddae47fd7d2379f175d2692c2e14e
Temirlan <tema.sebat@gmail.com>
2021-07-20 00:37:49 +0600

    testing

--- db.js
+++ db.js
@@ -21,9 +21,7 @@ client.query('SELECT * From groups;', (err, res) => {
   client.query('Select name from groups',(err,res)=>{
     if(err) throw err;
     console.log(res.JSON.parse());
-    client.end();
-    return res.JSON.parse();
-    
+    client.end(); 
   })
 };
 //  function queryAdd(val){
