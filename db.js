acc0d76229a64007b282d6cfc0eaf2a2d76085e2
Temirlan <tema.sebat@gmail.com>
2021-07-21 10:58:48 +0600

    testing

--- db.js
+++ db.js
@@ -9,23 +9,27 @@ const client = new Client({
 
 client.connect();
 
-// client.query('SELECT * From groups;', (err, res) => {
-//   if (err) throw err;
-//   for (let row of res.rows) {
-//     console.log(JSON.stringify(row));
-//   }
-//   client.end();
-// });
+client.query('SELECT * From groups;', (err, res) => {
+  if (err) throw err;
+  for (let row of res.rows) {
+    console.log(JSON.stringify(row));
+  }
+  client.end();
+});
 
 async function selectFrom() {
   try {
+    client.connect();
     const res = await client.query(
       `SELECT name FROM groups;`
     );
     return res.rows[0][data];
   } catch (err) {
     return err.stack;
+  } finally{
+    client.end();
   }
+
 }
 // module.exports.queryRead = queryRead;
 module.exports.selectFrom = selectFrom;
