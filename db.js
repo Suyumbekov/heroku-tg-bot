b16edffe02d17819f5710ce16e60a9f276d40f76
Temirlan <tema.sebat@gmail.com>
2021-07-21 12:39:25 +0600

    testing

--- db.js
+++ db.js
@@ -13,13 +13,12 @@ async function selectFrom() {
       const res = await client.query(
       `SELECT * FROM groups;`
     )
+    await client.end();
     return res.rows[0][data];
     
   } catch (err) {
     return err.stack;
-  } finally{
-    client.end();
-  }
+  } 
 }
 // module.exports.queryRead = queryRead;
 module.exports.selectFrom = selectFrom;
