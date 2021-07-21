538c03f41f1949088e7307fc9a8363038ec29db3
Temirlan <tema.sebat@gmail.com>
2021-07-21 11:32:01 +0600

    testing

--- db.js
+++ db.js
@@ -24,10 +24,7 @@ async function selectFrom() {
     return res.rows[0][data];
   } catch (err) {
     return err.stack;
-  } finally{
-    client.end();
   }
-
 }
 // module.exports.queryRead = queryRead;
 module.exports.selectFrom = selectFrom;
