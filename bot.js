8298437bf52191e080138f4ef5e9b762fc0527bb
Temirlan <tema.sebat@gmail.com>
2021-07-22 16:52:26 +0600

    testing

--- bot.js
+++ bot.js
@@ -8,7 +8,9 @@ var token = "1569313818:AAHm65XxYkgWDWipQMt0mf6VDUp4PZLICY4";
 // Подробнее: https://core.telegram.org/bots/api#getupdates
 var bot = new TelegramBot(token, { polling: true });
 let groups = [];
-groups = db.readQuery();
+Object.values(db.readQuery()).forEach(val =>{
+  groups.append(val);
+});
 bot.on("polling_error", console.log);
 
 bot.onText(/\/start/, (msg) => {
